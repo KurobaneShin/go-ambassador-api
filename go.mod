be15f298e5bf6bcec7b1507570ad0a9da96508da
icaro <darkflamemaster663@gmail.com>
2024-04-07 13:25:21 -0300

    register controller logic

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/gofiber/fiber/v2 v2.52.4
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -1,10 +1,68 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/asaskevich/govalidator"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+
+	"ambassador/src/database"
+	"ambassador/src/models"
+)
+
+type RegisterPayload struct {
+	FirstName       string `json:"first_name" valid:"type(string),required"`
+	LastName        string `json:"last_name" valid:"type(string),required"`
+	Email           string `json:"email" valid:"type(string),email,required"`
+	Password        string `json:"password" valid:"type(string),required"`
+	PasswordConfirm string `json:"password_confirm" valid:"type(string),required"`
+}
 
 func Register(ctx *fiber.Ctx) error {
-	return ctx.JSON(fiber.Map{
-		"message": "Register route",
-	})
+	var data map[string]string
 
+	if err := ctx.BodyParser(&data); err != nil {
+		return err
+	}
+
+	_, err := govalidator.ValidateStruct(RegisterPayload{
+		FirstName:       data["first_name"],
+		LastName:        data["last_name"],
+		Email:           data["email"],
+		Password:        data["password"],
+		PasswordConfirm: data["password_confirm"],
+	})
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest)
+
+		return ctx.JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	if data["password"] != data["password_confirm"] {
+		ctx.Status(fiber.StatusBadRequest)
+
+		return ctx.JSON(fiber.Map{
+			"message": "Password do not match",
+		})
+	}
+
+	password, err := bcrypt.GenerateFromPassword([]byte(data["password"]), 12)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest)
+	}
+
+	user := models.User{
+		Id:           uuid.New(),
+		FirstName:    data["first_name"],
+		LastName:     data["last_name"],
+		Email:        data["email"],
+		IsAmbassador: false,
+		Password:     string(password),
+	}
+
+	database.DB.Create(&user)
+
+	return ctx.JSON(user)
 }
